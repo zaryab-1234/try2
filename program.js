9f3f6892a6992e687b4d486ecf03f9ec21165ea4
zaryab-1234 <zaryabehsan5@gmail.com>
2025-09-28 03:55:54 +0500

    initial commit

--- program.js
+++ program.js
@@ -17,3 +17,42 @@ closeBtn.addEventListener('click', () => {
 
 // section 2 
 
+document.querySelector('.hero-button').addEventListener('click', function () {
+  // Replace with actual link or action
+  window.location.href = "/connect";
+});
+
+
+
+// section 3
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  
+  header.addEventListener('click', () => {
+    const currentlyActive = document.querySelector('.accordion-item.active');
+    
+    if (currentlyActive && currentlyActive !== item) {
+      currentlyActive.classList.remove('active');
+    }
+
+    item.classList.toggle('active');
+  });
+});
+
+
+// section 4
+
+
+
+
+
+
+
+// section 5
+ function openBox(type) {
+    alert("You clicked: " + type);
+    // You can replace this with:
+    // window.location.href = 'somefile.pdf';
+  }
